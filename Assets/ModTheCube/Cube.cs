42585e3f07516c3dd38deb211930a4b7ce0bc28c
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-07-07 02:47:08 +0530

    Initial commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+
+    
+    
+    void Start()
+    {
+        
+        transform.position = new Vector3(3, 4, 1);
+        InvokeRepeating("ColorChange", 0.5f,0.2f);
+        
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 10.0f*Time.deltaTime, 10.0f*Time.deltaTime);
+
+    }
+
+    void ColorChange()
+    {
+        Material material = Renderer.material;
+        float range = Random.Range(1.0f,10.0f);
+        transform.localScale = new Vector3(1,1,1) * (range/3.0f);
+        material.color = new Color(range, range, range, 90.0f);
+    }
+}
